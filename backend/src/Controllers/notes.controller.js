7fe4bab9f3989a9f80de26bc334d9d8c5b6e5691
Raahat Khan <raahatctrack001.rc@gmail.com>
2025-02-02 20:44:56 +0530

    add complete route and controller prototype for create note, search note get single note and all notes

---
+++ backend/src/Controllers/notes.controller.js
@@ -0,0 +1,17 @@
+import { asyncHandler } from "../Utils/asynHandler.js";
+
+export const createNote = asyncHandler(async (req, res, next)=>{
+
+})
+
+export const getNoteOfUser = asyncHandler(async (req, res, next)=>{
+
+})
+
+export const getAllNotesOfUser = asyncHandler(async (req, res, next)=>{
+
+})
+
+export const searchNotesOfUser = asyncHandler(async (req, res, next)=>{
+
+})
