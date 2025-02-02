d42d5ec6a76cec787c9c2d8bc76a7165ab456c1e
Raahat Khan <raahatctrack001.rc@gmail.com>
2025-02-02 22:01:42 +0530

    implement get single note of controller

--- backend/src/Routes/note.route.js
+++ backend/src/Routes/note.route.js
@@ -10,8 +10,8 @@ import { upload } from "../Middlewares/multer.middleware.js";
 const router = express.Router();
 
 router.route("/create-note").post(upload.none(), isUserLoggedIn, createNote);
-router.route("/get-notes").get(upload.none(), isUserLoggedIn, getAllNotesOfUser);
 router.route("/get-note/:noteId").get(upload.none(), isUserLoggedIn, getNoteOfUser);
+router.route("/get-notes").get(upload.none(), isUserLoggedIn, getAllNotesOfUser);
 router.route("/search-notes").post(upload.none(), isUserLoggedIn, searchNotesOfUser);
 
 export default router;
