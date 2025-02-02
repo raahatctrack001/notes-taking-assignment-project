ebea7ed1a97730d82e91bfef4978a3c3c22aac30
Raahat Khan <raahatctrack001.rc@gmail.com>
2025-02-02 21:14:46 +0530

    change code style to maintain consistency :async await and not .then .catch

--- backend/src/Controllers/auth.controller.js
+++ backend/src/Controllers/auth.controller.js
@@ -17,25 +17,19 @@ export const registerUser = asyncHandler(async (req, res, next)=>{
     try {
         if(userData.some(field=>field?.trim()?0:1)){
             throw new apiError(404, "All fields are necessary!");
-        }
-        
+        }       
         
         //creating new user
-        User
-            .create(req.body)
-            .then((newUser)=>{
-                if(!newUser){
-                    throw new apiError(401, "failed to register user");
-                }
-                console.log(newUser);
-                const {password, refreshToken, resetPasswordToken, ...data} = newUser._doc;
-                res
-                    .status(201)
-                    .json(
-                        new apiResponse(201, `welcome ${newUser.fullName}!!! Now you can keep your notes safely here.`, data)
-                    )
-                })
-            .catch(error=>next(error))
+        const newUser = await User.create(req.body);
+        if(!newUser){
+            throw new apiError(500, "failed to register user due to internal issues!")
+        }
+        const {password, refreshToken, resetPasswordToken, ...data} = newUser._doc;
+        res
+        .status(201)
+        .json(
+            new apiResponse(201, `welcome ${newUser.fullName}!!! Now you can keep your notes safely here.`, data)
+        )       
 
     } catch (error) {
         console.log(error)
