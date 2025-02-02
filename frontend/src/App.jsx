4c4be559e8dc1c9f831252b662d2205dbdec4de6
Raahat Khan <raahatctrack001.rc@gmail.com>
2025-02-02 22:47:35 +0530

    ad react router dom to set routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home"
 
 function App() {
-  return <div className='bg-black text-white'>
-    frontend part is here
-  </div>
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
